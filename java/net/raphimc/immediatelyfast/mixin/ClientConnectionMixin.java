2fd9a81e767a95cbd1d14e072b7773438c892ec2
didor00 <122474439+didor00@users.noreply.github.com>
2026-07-07 14:13:03 +0300

    Add files via upload

---
+++ java/net/raphimc/immediatelyfast/mixin/ClientConnectionMixin.java
@@ -0,0 +1,39 @@
+package net.raphimc.immediatelyfast.mixin;
+
+import net.raphimc.immediatelyfast.event.EventManager;
+import net.raphimc.immediatelyfast.event.events.PacketReceiveListener;
+import net.raphimc.immediatelyfast.event.events.PacketSendListener;
+import net.raphimc.immediatelyfast.utils.NetworkUtils;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.listener.PacketListener;
+import net.minecraft.network.packet.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin {
+
+	@Inject(method = "handlePacket", at = @At("HEAD"), cancellable = true)
+	private static <T extends PacketListener> void onPacketReceive(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
+		PacketReceiveListener.PacketReceiveEvent event = new PacketReceiveListener.PacketReceiveEvent(packet);
+
+		EventManager.fire(event);
+		if (event.isCancelled()) ci.cancel();
+	}
+
+	@Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"), cancellable = true)
+	private void onPacketSend(Packet<?> packet, CallbackInfo ci) {
+		// Skip event firing for silent packets
+		if (NetworkUtils.isSilentPacket()) {
+			return;
+		}
+		
+		PacketSendListener.PacketSendEvent event = new PacketSendListener.PacketSendEvent(packet);
+
+		EventManager.fire(event);
+		if (event.isCancelled()) ci.cancel();
+	}
+}
